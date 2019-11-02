9cd01d3e7e65e06bee6cdec796216cece51ffbba
dylanbobb <dylan.bobb@hotmail.com>
2019-11-02 13:25:50 -0400

    Quantum Class

---
+++ McGillPhys19/src/mcgillphys19/FXMLDocumentController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcgillphys19;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Dylan
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // 
+        double[][] q1s = {{0},{1}};
+        System.out.println(q1s[1]);
+//        Qubit q1 = new Qubit({1},{0});
+    }    
+    
+}
