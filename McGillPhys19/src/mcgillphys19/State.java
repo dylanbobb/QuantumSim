25178f431b12c2cc13455d9302fbcbde9b57279b
Mohamed <>
2019-11-02 18:34:08 -0400

    edited swap to work

--- McGillPhys19/src/mcgillphys19/State.java
+++ McGillPhys19/src/mcgillphys19/State.java
@@ -7,7 +7,6 @@ public class State {
 
     protected double[] state;
     protected int nbQubits;
-    
 
     public State(int nbQubits) {
         this.state = new double[(int) Math.pow(2, nbQubits)];
@@ -23,22 +22,19 @@ public class State {
     public double[] getState() {
         return this.state;
     }
-    
-    public void h(int[] selection)
-    {
+
+    public void h(int[] selection) {
         int currSelectionIndex = 0;
         int selectionLen = selection.length;
         double[][] operationChain = null;
         if (selection[currSelectionIndex] == 0) {
             operationChain = H;
             currSelectionIndex++;
-        }
-        else {
+        } else {
             operationChain = I;
         }
         System.out.println(Arrays.deepToString(operationChain));
-        for(int i = 1; i < nbQubits; i++)
-        {
+        for (int i = 1; i < nbQubits; i++) {
             if (currSelectionIndex < selectionLen && selection[currSelectionIndex] == i) {
                 operationChain = Matrix.tensorMatrix(operationChain, H);
                 currSelectionIndex++;
@@ -47,29 +43,49 @@ public class State {
             }
         }
         System.out.println(Arrays.deepToString(operationChain));
-        
+
         this.state = Matrix.transform(operationChain, this.getState());
     }
 
-    public void swap() {
-        this.state = Matrix.transform(S, this.getState());
+    public void swap(int a, int b) {
+        int source = a < b ? a : b;
+        int dest = a < b ? b : a;
+
+        double[][][] transformations = new double[dest - source][1][1];
+        for (int i = 0; i < transformations.length; i++) {
+            
+            transformations[i][0][0] = 1.0;
+            for (int j = 0; j < i + source; j++) {
+                transformations[i] = Matrix.tensorMatrix(transformations[i], I);
+            }
+            transformations[i] = Matrix.tensorMatrix(transformations[i], S);
+            for (int j = i+source+1; j < nbQubits - 1; j++) {
+                transformations[i] = Matrix.tensorMatrix(transformations[i], I);
+            }
+            
+            state = Matrix.transform(transformations[i], state);
+        }
+        
+        for (int i = transformations.length - 2; i >= 0; i--) {
+            state = Matrix.transform(transformations[i], state);
+        }
     }
 
     public void cNot() {
         double[][] cNot = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 1}, {0, 0, 1, 0}};
         this.state = Matrix.transform(cNot, this.getState());
     }
-    
+
     public void collapse(Random rand) {
         double result = rand.nextDouble();
-        
+
         double[] newState = new double[state.length];
-        
+
         double currState = 0;
-        
+
         for (int i = 0; i < state.length; i++) {
             currState += state[i] * state[i];
-            
+
             if (result <= currState) {
                 newState[i] = 1;
                 state = newState;
@@ -77,8 +93,8 @@ public class State {
             }
         }
     }
-    
-    public final double[][] H = { {1/Math.sqrt(2),1/Math.sqrt(2)}, {1/Math.sqrt(2),-1/Math.sqrt(2)} };
-    public final double[][] I = { {1,0}, {0,1} };
+
+    public final double[][] H = {{1 / Math.sqrt(2), 1 / Math.sqrt(2)}, {1 / Math.sqrt(2), -1 / Math.sqrt(2)}};
+    public final double[][] I = {{1, 0}, {0, 1}};
     public final double[][] S = {{1, 0, 0, 0}, {0, 0, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 1}};
 }
